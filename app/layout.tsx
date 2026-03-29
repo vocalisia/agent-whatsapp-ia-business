57e993b927996fd315c3de55114ac61525703002
vocalisia <cohenrichard07@gmail.com>
2026-03-29 23:11:45 +0200

    feat: add Google Analytics G-1Q10Z6C916

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,12 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1Q10Z6C916" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-1Q10Z6C916');`,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
